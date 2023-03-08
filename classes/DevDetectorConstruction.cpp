1e33705e65741259cbba2f58a3f629a1d76b8a26
Conner Grice <connergrice12@gmail.com>
2023-03-08 19:20:13 +0100

    Refactor Stave creation

--- classes/DevDetectorConstruction.cpp
+++ classes/DevDetectorConstruction.cpp
@@ -8,7 +8,8 @@
 #include "DevDetectorConstruction.h"
 
 DevDetectorConstruction::DevDetectorConstruction():HCIWidth(15*mm),
-staveLength(320*mm),HCILength(271.2*mm),HCIUnitThickness(285*um),angle(M_PI/3.0),sides(6){
+staveLength(320*mm),HCILength(271.2*mm),HCIUnitThickness(285*um),
+sides(6),angle(M_PI/3.0),plateThickness(240*um){
 	//Defines that maertial variables
 	DefineMaterials();
 }
@@ -31,17 +32,15 @@ void DevDetectorConstruction::DefineMaterials(){
 }
 
 G4LogicalVolume* DevDetectorConstruction::staveMother(G4double width){
-	G4double thickness = 565*um;
-
+	G4double thickness = 850*um;
 	G4Box* staveS = new G4Box("StaveS",width,thickness/2,staveLength/2);
-	G4LogicalVolume* staveL = new G4LogicalVolume(staveS,pWorldMat,"StaveL");
+	G4LogicalVolume* staveOut = new G4LogicalVolume(staveS,pWorldMat,"StaveL");
 
-	return staveL;
+	return staveOut;
 }
 
 G4LogicalVolume* DevDetectorConstruction::coldPlate(G4double width) {
-	G4double thickness = 240*um;
-	G4Box* plateS = new G4Box("PlateS",width,thickness/2,staveLength/2);
+	G4Box* plateS = new G4Box("PlateS",width,plateThickness/2,staveLength/2);
 	G4LogicalVolume* plateL = new G4LogicalVolume(plateS,pPlateMat,"PlateL");
 
 	return plateL;
@@ -49,11 +48,24 @@ G4LogicalVolume* DevDetectorConstruction::coldPlate(G4double width) {
 
 G4LogicalVolume* DevDetectorConstruction::fleece(G4double width) {
 	G4double thickness = 20*um;
-
 	G4Box* fleeceS = new G4Box("FleeceS",width,thickness/2,staveLength/2);
 	G4LogicalVolume* fleeceL = new G4LogicalVolume(fleeceS,pFleeceMat,"FleeceL");
 
 	return fleeceL;
+}
+
+void DevDetectorConstruction::buildPlate(G4double width) {
+	G4double offset = plateThickness/2+10*um;
+
+	G4LogicalVolume* plateL = coldPlate(width);
+	G4LogicalVolume* fleeceL = fleece(width);
+
+	//Bottom fleece layer
+	new G4PVPlacement(0,G4ThreeVector(0,-offset,0),fleeceL,"FleecePBot",staveL,false,2);
+	//Cold plate layer
+	new G4PVPlacement(0,G4ThreeVector(0,0,0),plateL,"PlateP",staveL,false,1);
+	//Top fleece layer
+	new G4PVPlacement(0,G4ThreeVector(0,offset,0),fleeceL,"FleecePTot",staveL,false,3);
 }
 
 G4LogicalVolume* DevDetectorConstruction::HCIUnitMother() {
@@ -64,7 +76,6 @@ G4LogicalVolume* DevDetectorConstruction::HCIUnitMother() {
 }
 
 G4LogicalVolume* DevDetectorConstruction::HCILayerMother(G4double thickness,G4String name) {
-
 	G4Box* stripS = new G4Box(name+"S",HCIWidth/2.0,thickness/2.0,HCILength/2.0);
 	G4LogicalVolume* stripL = new G4LogicalVolume(stripS,pWorldMat,name+"L");
 
@@ -73,7 +84,6 @@ G4LogicalVolume* DevDetectorConstruction::HCILayerMother(G4double thickness,G4St
 
 G4LogicalVolume* DevDetectorConstruction::HCISegment(G4double thickness,G4String name,G4Material* material) {
 	G4double length = HCILength/9.0;
-
 	G4Box* segmentS = new G4Box(name+"SegS",HCIWidth/2.0,thickness/2.0,length/2.0);
 	G4LogicalVolume* segmentL = new G4LogicalVolume(segmentS,material,name+"SegL");
 
@@ -88,15 +98,14 @@ void DevDetectorConstruction::buildHCILayer(G4String name, G4double thickness, G
 	new G4PVPlacement(0,pos+G4ThreeVector(0,thickness/2,0),layerStripL,name+"StripP",HCIUnitL,false,100);
 }
 
-void DevDetectorConstruction::buildHCI(G4String name) {
-	G4LogicalVolume* unitL = HCIUnitMother();
+void DevDetectorConstruction::buildHCI() {
 	G4ThreeVector height= G4ThreeVector(0,-HCIUnitThickness/2,0);
 
 	std::vector<G4double> thicknesses {40*um,50*um,40*um,30*um,10*um,75*um,10*um,30*um};
 	std::vector<G4String> names {"GlueA","Chips","GlueB","SolderA","ConductingA","Substrate","ConductingB","SolderB"};
 	std::vector<G4Material*> materials {pGlue,pChips,pGlue,pSolder,pConducting,pSubstrate,pConducting,pSolder};
 
-	for (int i=0; i<thicknesses.size();i++) {
+	for (unsigned int i=0; i<thicknesses.size();i++) {
 		buildHCILayer(names[i],thicknesses[i],materials[i],height);
 		height += G4ThreeVector(0,thicknesses[i],0);
 	}
@@ -104,69 +113,46 @@ void DevDetectorConstruction::buildHCI(G4String name) {
 }
 
 void DevDetectorConstruction::ConstructStaves(G4int numOfHCIs,G4String name) {
-/*
-	//Constants for all staves
-	const G4double HCIWidth = 15.0*mm;
-	const G4double plateLength = 160.0*mm;
-
-	const G4double angle = M_PI/3.0;
-	const G4int sides = 6;
-	*/
-	const G4double plateThickness = 120*um;
 	//Parameter calculations
 	const G4double staveWidth = getPlateWidth(numOfHCIs);
-	const G4double plateRadius = getPlateRadius(staveWidth,plateThickness);
+	const G4double plateRadius = getPlateRadius(staveWidth);
 
-	G4ThreeVector staveBottom = G4ThreeVector(0,-282.5*um,0);
+	//Initialise stave mother volume
+	staveL = staveMother(staveWidth);
 
-	G4LogicalVolume* staveL = staveMother(staveWidth);
-	G4LogicalVolume* plateL = coldPlate(staveWidth);
-	G4LogicalVolume* fleeceL = fleece(staveWidth);
+	//Creates and places cold plate and fleece
+	buildPlate(staveWidth);
+
+	//Initialise HCI mother volume
 	HCIUnitL = HCIUnitMother();
-	buildHCI("HCI1");
 
+	//Creates and places entire HCI unit
+	buildHCI();
 
+	//Left-most HCI unit position
+	G4ThreeVector HCIPosition = G4ThreeVector(-(HCIWidth*numOfHCIs/2)-HCIWidth/2,0,0);
+	G4RotationMatrix* HCIRotation = new G4RotationMatrix;
 
+	//Places each HCI unit on alternating side of the plate
+	for (int i=0;i < numOfHCIs;i++){
+		//Slide to the right
+		HCIPosition += G4ThreeVector(HCIWidth,0,0);
+		if (i%2 == 0) {
+			//Place on top
+			HCIPosition.setY(282.5*um);
+			HCIRotation = new G4RotationMatrix(0,0,0);
+		} else {
+			//Place on bottom and rotate
+			HCIPosition.setY(-282.5*um);
+			HCIRotation = new G4RotationMatrix(0,0,M_PI);
+		}
 
-	G4VPhysicalVolume* fleecePBot = new G4PVPlacement(0,staveBottom+G4ThreeVector(0,10*um,0),fleeceL,"FleecePBot",staveL,false,2);
-	G4VPhysicalVolume* plateP = new G4PVPlacement(0,staveBottom+G4ThreeVector(0,140*um,0),plateL,"PlateP",staveL,false,1);
-	G4VPhysicalVolume* fleecePTot = new G4PVPlacement(0,staveBottom+G4ThreeVector(0,270*um,0),fleeceL,"FleecePTot",staveL,false,3);
-	G4VPhysicalVolume* HCIUnit1 = new G4PVPlacement(0,G4ThreeVector(0,140*um,0),HCIUnitL,"HCI1",staveL,false,4);
-
-	G4VPhysicalVolume* staveP = new G4PVPlacement(0,G4ThreeVector(0,0,0),staveL,"StaveP",pLogicalWorld,false,5);
-
-/*
-	//Initialise assembly
-	G4AssemblyVolume* pStave = new G4AssemblyVolume();
-
-	//Initial position of Stave section inside assembly
-	G4ThreeVector platePosition = G4ThreeVector(plateRadius,0*mm,0*mm);
-	G4RotationMatrix* rotation = new G4RotationMatrix(0,0,M_PI/2);
-
-	//Defining a single cold plate.
-	G4Box* pSolidPlate = new G4Box(name+"S",plateWidth,plateThickness,plateLength);
-	G4LogicalVolume* pLogicalPlate = new G4LogicalVolume(pSolidPlate,pPlateMat,name+"L");
-
-	//Add geometery to assembly
-	pStave->AddPlacedVolume(pLogicalPlate,platePosition,rotation);
-
-	//Variables used to move assembly
-	G4RotationMatrix* rotateAssembly = new G4RotationMatrix;
-	G4ThreeVector translateAssembly;
-
-	G4RotationMatrix* rotateHCI = new G4RotationMatrix(0,0,M_PI/2);
-	G4ThreeVector translateHCI = G4ThreeVector(plateRadius,0,0);
-
-	//Moves each cold plate to the correct position
-	for (int i = 0; i < sides; i++) {
-		//MOve and paste assembly
-		pStave->MakeImprint(pLogicalWorld,translateAssembly,rotateAssembly);
-		//Rotate assembly
-		rotateAssembly->rotateZ(angle);
-		//NOTE: Assembly is centered at the center of the world so it does not need to be translated
-
+		//Place HCI Unit volume in simulation
+		new G4PVPlacement(HCIRotation,HCIPosition,HCIUnitL,"HCI"+(G4String)i,staveL,false,(10*numOfHCIs)+i);
 	}
-*/
+
+	//Place lowest level stave mother volume
+	new G4PVPlacement(0,G4ThreeVector(0,0,0),staveL,"Stave"+name+"P",pLogicalWorld,false,5);
 }
 
 G4VPhysicalVolume* DevDetectorConstruction::Construct(){
@@ -176,8 +162,8 @@ G4VPhysicalVolume* DevDetectorConstruction::Construct(){
 	G4PVPlacement* pPhysicalWorld = new G4PVPlacement(0,G4ThreeVector(0,0,0),pLogicalWorld,"WorldP",0,false,0,true);
 
 	//Generate staves
-	//ConstructStaves(3,"StaveC");
-	ConstructStaves(4,"StaveD");
+	ConstructStaves(3,"C");
+	//ConstructStaves(4,"StaveD");
 	//ConstructStaves(2,"StaveB");
 
 	return pPhysicalWorld;
